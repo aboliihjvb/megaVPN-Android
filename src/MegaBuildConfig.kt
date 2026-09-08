d4d3a056357a2b113ff3f1b71ca8c08c3d0fc0fd
Abolfazl_MKT <boromandabolfazl38@gmail.com>
2026-09-08 13:59:32 +0330

    test: add temporary subscription config

---
+++ src/MegaBuildConfig.kt
@@ -0,0 +1,6 @@
+package com.andrew.proxyapp
+
+/** Temporary test-only subscription. Move this to the backend before public release. */
+object MegaBuildConfig {
+    const val SUBSCRIPTION_URL: String = "https://l-g1ig3b-iib2ew47wk81-m0.gw01dsvw5nzq4vpfa4m8hhyr.workers.dev/Kl4Ey2Az8BZyN/sub/normal?app=xray#%F0%9F%92%A6%20BPB%20Normal"
+}
